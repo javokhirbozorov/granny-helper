a058abbeeb47024e86b4c73a75426dea12a11f38
Rita <mrgaysina@gmail.com>
2022-09-06 16:34:16 +0300

    login view

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const loginRouter = require('./src/routes/loginRoute');
+
 app.get('/', (req, res) => {
   res.send('<h1>HOME</h1>');
 });
+
+app.use('/login', loginRouter);
 
 app.listen(PORT, () => {
   console.log('server has started on port:', PORT);
