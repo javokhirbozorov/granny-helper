076c3b282b7137979d277376a75d6331eb2e9860
Rita <mrgaysina@gmail.com>
2022-09-07 09:29:20 +0300

    auth done

--- src/views/LogIn.jsx
+++ src/views/LogIn.jsx
@@ -6,15 +6,15 @@ function LogIn({ props }) {
   return (
     <Layout>
       <div className="d-flex justify-content-center align-items-center w-50 p-3">
-        <form action="/login">
+        <form action="/login" method="POST">
           <div className="mb-3">
             <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            <input type="email" className="form-control" id="loginuser" name="email" aria-describedby="emailHelp" />
             <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
           </div>
           <div className="mb-3">
             <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <input type="password" className="form-control" id="loginpass" name="password" />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
         </form>
