d23b135af7e6d9b0cdea2cda87cfde87c6b49fbd
BlackAbyssFox <90621864+BlackAbyssFox@users.noreply.github.com>
2022-09-07 09:21:29 +0300

    grandchildMain1

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('@babel/register');
+require('dotenv').config();
 
 // npm
 const express = require('express');
 const path = require('path');
-
+const { sequelize } = require('./db/models');
 
 // express init
 const app = express();
@@ -14,7 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
 
-
 // import routes
 const loginRouter = require('./src/routes/loginRoute');
 const signUpRoute = require('./src/routes/signUpRoute');
@@ -23,10 +23,10 @@ const MainPage = require('./src/routes/mainPageRoute');
 const GrannyMainPage = require('./src/routes/homeGrannyRoute');
 const GrannyProfile = require('./src/routes/grannyProfileRoute');
 
+const grandchildRouter = require('./src/routes/grandchildRouter');
 
 // App Main Address
 const granny = 'granny.com';
-
 
 // init routes
 app.use('/sign-up', signUpRoute);
@@ -35,10 +35,16 @@ app.use(`/${granny}/main`, GrannyMainPage);
 app.use(`/${granny}/profile`, GrannyProfile);
 
 app.use('/login', loginRouter);
-
+app.use('/', grandchildRouter);
 
 // app listener
 
-app.listen(PORT, () => {
-  console.log(`Server has started on port: ${PORT}\n`);
+app.listen(PORT, async () => {
+  console.log(`Server starting on PORT => ${PORT}`);
+  try {
+    await sequelize.authenticate();
+    console.log('Connect sequelize');
+  } catch (error) {
+    console.error(error);
+  }
 });
