47b5ced676f06981ed63f34f9059f371a0d5ec98
Ayub <75487903+ayub-a@users.noreply.github.com>
2022-09-08 20:36:46 +0300

    grandChildProfile

--- app.js
+++ app.js
@@ -7,16 +7,21 @@ const FileStore = require('session-file-store')(session);
 // npm
 const express = require('express');
 const path = require('path');
-const { sequelize } = require('./db/models');
+
+// Database
+const { sequelize } = require('./db/models')
+
 
 // express init
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
 // express settings
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.json());
+
 
 // создание конфига для куки
 const sessionConfig = {
@@ -33,17 +38,17 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 
+
 // import routes
 const loginRouter = require('./src/routes/loginRoute');
 const signUpRoute = require('./src/routes/signUpRoute');
-const logoutRoute = require('./src/routes/logoutRoute');
+const logoutRoute = require('./src/routes/logoutRoute')
 
 const MainPage = require('./src/routes/mainPageRoute');
 const GrannyMainPage = require('./src/routes/homeGrannyRoute');
 const GrannyProfile = require('./src/routes/grannyProfileRoute');
-const GrannyProfile1 = require('./src/routes/grannyProfileRouter1');
+const GrandChildProfileRoute = require('./src/routes/grandChildProfileRoute');
 
-// const { sequelize } = require('./db/models');
 
 const grandchildRouter = require('./src/routes/grandchildRouter');
 
@@ -51,22 +56,22 @@ const grandchildRouter = require('./src/routes/grandchildRouter');
 const granny = 'granny.com';
 
 // init routes
-app.use('/sign-up', signUpRoute);
 app.use('/', MainPage);
 app.use(`/${granny}/main`, GrannyMainPage);
 app.use(`/${granny}/profile`, GrannyProfile);
+app.use(`/grandChildProfile`, GrandChildProfileRoute);
+
 
 app.use('/login', loginRouter);
+app.use('/signin', signUpRoute);
 // ручка для выхода пользователя с уничтожением куки и файла сессии
 app.use('/logout', logoutRoute);
 
-app.use('/grandchild', grandchildRouter);
+app.use('/', grandchildRouter);
 
-app.use('/', GrannyProfile1);
-
-// app listener
 
 app.listen(PORT, async () => {
+
   console.log(`Server starting on PORT => ${PORT}`);
   try {
     await sequelize.authenticate();
